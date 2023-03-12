fb2517c89d1d1ea326d9a976ecbb754563dc0e24
certainly1182 <fawcettjames63@gmail.com>
2023-03-12 19:32:54 +0000

    fix elvis operator

--- src/main/java/me/certainly1182/fastminecarts/FastMinecarts.kt
+++ src/main/java/me/certainly1182/fastminecarts/FastMinecarts.kt
@@ -11,6 +11,7 @@ import org.bukkit.event.vehicle.VehicleMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class FastMinecarts : JavaPlugin(), Listener {
+    private val VANILLA_MAX_SPEED = 0.4
     private var _blockMaxSpeeds = mutableMapOf<Material, Double>()
     private val railTypes = listOf(
         Material.RAIL, Material.POWERED_RAIL,
@@ -43,7 +44,7 @@ class FastMinecarts : JavaPlugin(), Listener {
         if (railBlock.type !in railTypes) return
 
         val blockBelow = railBlock.getRelative(0, -1, 0)
-        val blockMultiplier = _blockMaxSpeeds[blockBelow.type] ?: 1.0
+        val blockMultiplier = _blockMaxSpeeds[blockBelow.type] ?: VANILLA_MAX_SPEED
         minecart.maxSpeed = blockMultiplier
     }
 
@@ -53,8 +54,8 @@ class FastMinecarts : JavaPlugin(), Listener {
         if (event.exited !is Player) return
 
         val minecart = event.vehicle as Minecart
-        if (minecart.maxSpeed > 0.4) {  // vanilla max speed
-            minecart.maxSpeed = 0.4
+        if (minecart.maxSpeed > VANILLA_MAX_SPEED) {
+            minecart.maxSpeed = VANILLA_MAX_SPEED
         }
     }
 }
