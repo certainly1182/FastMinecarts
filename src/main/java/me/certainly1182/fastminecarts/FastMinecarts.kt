e1ce351859372bc3c5061b45c0e81e185d37fc89
woliver99 <oliver_giroux@hotmail.com>
2023-09-16 10:46:10 -0400

    Default Speed, Reload Config, Spigot Plugin
    You can now set a default speed that the minecarts should go.
    
    Added a /FastMinecarts Reload Command that uses the permission: "FastMinecarts.reload" but it can also be ran by an op.
    
    It now runs on the spigot API because there was no need for the paper one and gives more compatibility for other plugins

--- src/main/java/me/certainly1182/fastminecarts/FastMinecarts.kt
+++ src/main/java/me/certainly1182/fastminecarts/FastMinecarts.kt
@@ -2,6 +2,10 @@ package me.certainly1182.fastminecarts
 
 import org.bukkit.Bukkit
 import org.bukkit.Material
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.command.TabCompleter
 import org.bukkit.entity.Minecart
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
@@ -10,19 +14,22 @@ import org.bukkit.event.vehicle.VehicleExitEvent
 import org.bukkit.event.vehicle.VehicleMoveEvent
 import org.bukkit.plugin.java.JavaPlugin
 
-class FastMinecarts : JavaPlugin(), Listener {
-    private val VANILLA_MAX_SPEED = 0.4
+class FastMinecarts : JavaPlugin(), Listener, CommandExecutor, TabCompleter {
+    private var VANILLA_MAX_SPEED = 0.4
     private var _blockMaxSpeeds = mutableMapOf<Material, Double>()
     private val railTypes = listOf(
-        Material.RAIL, Material.POWERED_RAIL,
-        Material.DETECTOR_RAIL, Material.ACTIVATOR_RAIL
+            Material.RAIL, Material.POWERED_RAIL,
+            Material.DETECTOR_RAIL, Material.ACTIVATOR_RAIL
     )
     override fun onEnable() {
         saveDefaultConfig()
         loadConfig()
         Bukkit.getPluginManager().registerEvents(this, this)
+        getCommand("FastMinecarts")?.setExecutor(this)
+        getCommand("FastMinecarts")?.tabCompleter = this
     }
     private fun loadConfig() {
+        VANILLA_MAX_SPEED = config.getDouble("default-speed", 0.4)
         val blockConfig = config.getConfigurationSection("blocks") ?: return
         _blockMaxSpeeds.clear()
         for (key in blockConfig.getKeys(false)) {
@@ -57,5 +64,30 @@ class FastMinecarts : JavaPlugin(), Listener {
         if (minecart.maxSpeed > VANILLA_MAX_SPEED) {
             minecart.maxSpeed = VANILLA_MAX_SPEED
         }
+    }
+
+    override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<String>): Boolean {
+        if (command.name.equals("FastMinecarts", ignoreCase = true)) {
+            if (sender.hasPermission("FastMinecarts.reload")) {
+                if (args.isNotEmpty() && args[0].equals("reload", ignoreCase = true)) {
+                    reloadConfig()
+                    loadConfig()
+                    sender.sendMessage("§aFastMinecarts configuration reloaded.")
+                    return true
+                }
+            } else {
+                sender.sendMessage("§cYou do not have permission to reload FastMinecarts.")
+            }
+        }
+        return false
+    }
+
+    override fun onTabComplete(sender: CommandSender, command: Command, alias: String, args: Array<String>): List<String>? {
+        if (command.name.equals("FastMinecarts", ignoreCase = true)) {
+            if (args.size == 1) {
+                return listOf("reload").filter { it.startsWith(args[0], ignoreCase = true) }
+            }
+        }
+        return null
     }
 }
